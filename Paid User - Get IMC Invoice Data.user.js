ea122b9cef52c3fd460c6f93f856f962169d9230
dushyantsaharan <34511321+dushyantsaharan@users.noreply.github.com>
2017-12-14 16:45:57 +0530

    Update Paid User - Get IMC Invoice Data.user.js

--- Paid User - Get IMC Invoice Data.user.js
+++ Paid User - Get IMC Invoice Data.user.js
@@ -13,6 +13,7 @@
 
 (function() {
     var amt = document.getElementById("MainContent_myForm_txtPayable_amt").value;
+    var client_code = document.getElementById("MainContent_myForm_txtCode1").value;
 
     $('#MainContent_myForm_pnlTaxtype').hide();
     $('#MainContent_myForm_pnlSaletype').hide();
@@ -49,7 +50,7 @@
     $("#MainContent_myForm_pnlFooter1 .form-horizontal").append(markuptabledata);
 
     var no = document.getElementById("MainContent_myForm_txtCinvoice_no").value;
-    $.get("http://client.saharanherbal.com/fnGetData.php?no="+no, function(data)
+    $.get("http://client.saharanherbal.com/fnGetData.php?client_code="+client_code+"no="+no, function(data)
     {
         var response = JSON.parse(data);
         if (response.status === "saved")
